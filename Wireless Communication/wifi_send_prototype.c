54f07f8e93a53ced20bc653d909143e643375602
anwelch0 <97703105+anwelch0@users.noreply.github.com>
2022-01-25 16:46:08 -0500

    Implemented Arduino Enable Interrupts
    Implemented interrupts into the Arduino to enable WiFi Transmission, turning it off after 5 seconds of no change detected on Digital Pin 2.

--- Wireless Communication/wifi_send_prototype.c
+++ Wireless Communication/wifi_send_prototype.c
@@ -10,6 +10,11 @@ WifiUDP UDP;
 char packet[100];
 char reply[] = "Packet received.";
 
+bool wifiEn = false;
+unsigned long resetTime = 0;
+const unsigned long MSCUTOFF = 5000;
+//will determine if wireless needs to be shut off
+
 string sensorData;
 
 void UDPsetup(){
@@ -41,14 +46,32 @@ void sendPacket(string destIP, string destPort, string sensorData){
 	UDP.endPacket();
 }
 
+void wirelessEnable(){
+	wifiEn = true;
+	resetTime = millis();
+}
+
 //TODO: Enable interrupts in this file and turn on transmission ONLY when interrupts are triggered
+// make use of attachInterrupt(digitalPinToInterrupt(pin), ISR, mode)
+// Consider using a boolean ENABLE value, activated during the ISR
+// Will be triggered upon change of pins 2 or 3, so call will probably look like 
+//attachInterrupt(digitalPinToInterrupt(2), wirelessEnable(), CHANGE);
 //TODO: Create method to read & process the sensors in its own header and .c file
 
 int main(){
 	UDPsetup();
+	attachInterrupt(digitalPinToInterrupt(2), wirelessEnable(), CHANGE);
+	//sets up interrupt to enable wifi, detects change on digital pin 2
+	
 	while(1){
-		sendPacket("some ip", "some udp port" sensorData); //should be the ip adress and port of the computer with the display program
-		delay(5000);
+		if(wifiEn){
+			sendPacket("some ip", "some udp port" sensorData); //should be the ip adress and port of the computer with the display program
+			delay(5000);
+		}
+		if(millis() - resetTime >= MSCUTOFF){
+			wifiEn = false;
+		}
+		//calls millis() every loop, if difference is over 5s, turn off wifi
 	}
 	
 	return 0;
