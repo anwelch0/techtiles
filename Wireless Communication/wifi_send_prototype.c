7476a6e603b955c1d8563eb8ee0c4d7f95c06b9b
anwelch0 <97703105+anwelch0@users.noreply.github.com>
2022-02-08 13:30:33 -0500

    Update wifi_send_prototype.c
    numerous bug and syntax fixes

--- Wireless Communication/wifi_send_prototype.c
+++ Wireless Communication/wifi_send_prototype.c
@@ -1,12 +1,17 @@
+#include <dummy.h>
+
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include <string>
 
-#define WIFI_SSID "some network" //will be the hypothetical hospital's wifi
-#define WIFI_PASS "some password"
-#define UDP_PORT 4210
+using namespace std;
+
+const char* WIFI_SSID = "some network";
+const char* WIFI_PASS = "some password";
+uint16_t UDP_PORT = 4210;
 
 //define udp
-WifiUDP UDP;
+WiFiUDP UDP;
 char packet[100];
 char reply[] = "Packet received.";
 
@@ -18,37 +23,37 @@ const unsigned long MSCUTOFF = 5000;
 string sensorData;
 
 void UDPsetup(){
-	//set up serial
-	Serial.begin(115200);
-	Serial.println();
-	
-	//turn on WiFi
-	WiFi.begin(WIFI_SSID, WIFI_PASS);
-	Serial.print("Connecting to ");
-	Serial.print(WIFI_SSID);
-	//wait for WiFi to connect
-	while(WiFi.status() !- WL_CONNECTED){
-		delay(100);
-		Serial.print(".");
-	}
-	Serial.println();
-	Serial.print("Connected to IP address: ");
-	Serial.println(WiFi.localIP());
-	//turn on UDP
-	UDP.begin(UDP_PORT);
-	Serial.print("Awaiting input from UDP port: ");
-	Serial.println(UDP_PORT);
+  //set up serial
+  Serial.begin(115200);
+  Serial.println();
+  
+  //turn on WiFi
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+  //wait for WiFi to connect
+  while(WiFi.status() != WL_CONNECTED){
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected to IP address: ");
+  Serial.println(WiFi.localIP());
+  //turn on UDP
+  UDP.begin(UDP_PORT);
+  Serial.print("Awaiting input from UDP port: ");
+  Serial.println(UDP_PORT);
 }
 
-void sendPacket(string destIP, string destPort, string sensorData){
-	UDP.beginPacket(destIP, destPort);
-    	UDP.write(sensorData);
-	UDP.endPacket();
+void sendPacket(char *destIP, uint16_t destPort, string sensorData){
+  UDP.beginPacket(destIP, destPort);
+    UDP.write(sensorData);
+    UDP.endPacket();
 }
 
 void wirelessEnable(){
-	wifiEn = true;
-	resetTime = millis();
+  wifiEn = true;
+  resetTime = millis();
 }
 
 //TODO: Enable interrupts in this file and turn on transmission ONLY when interrupts are triggered
@@ -56,23 +61,26 @@ void wirelessEnable(){
 // Consider using a boolean ENABLE value, activated during the ISR
 // Will be triggered upon change of pins 2 or 3, so call will probably look like 
 //attachInterrupt(digitalPinToInterrupt(2), wirelessEnable(), CHANGE);
+
 //TODO: Create method to read & process the sensors in its own header and .c file
 
 int main(){
-	UDPsetup();
-	attachInterrupt(digitalPinToInterrupt(2), wirelessEnable(), CHANGE);
-	//sets up interrupt to enable wifi, detects change on digital pin 2
-	
-	while(1){
-		if(wifiEn){
-			sendPacket("some ip", "some udp port" sensorData); //should be the ip adress and port of the computer with the display program
-			delay(5000);
-		}
-		if(millis() - resetTime >= MSCUTOFF){
-			wifiEn = false;
-		}
-		//calls millis() every loop, if difference is over 5s, turn off wifi
-	}
-	
-	return 0;
+  
+  UDPsetup();
+  attachInterrupt(digitalPinToInterrupt(2), wirelessEnable, CHANGE);
+  //sets up interrupt to enable wifi, detects change on digital pin 2
+  
+  while(1){
+    if(wifiEn){
+      sendPacket("some ip", 4210, sensorData);
+      delay(5000);
+    }
+    
+    if(millis() - resetTime >= MSCUTOFF){
+      wifiEn = false;
+    }
+    //calls millis() every loop, if difference is over 5s, turn off wifi
+  }
+  
+  return 0;
 }
